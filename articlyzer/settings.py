cde764a929957157ab1bc903fc92212ac589efe3
Indrani Biswas <indranibiswas@Indranis-MacBook-Air.local>
2020-05-11 20:42:57 +0530

    modified settings file

--- articlyzer/settings.py
+++ articlyzer/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoise.Middleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,6 +124,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'articlyzer/static/')
 ]
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
