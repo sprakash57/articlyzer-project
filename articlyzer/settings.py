fdd0ad4ff8423ddcdb314f21020e2c70768142ea
Indrani Biswas <indranibiswas@Indranis-MacBook-Air.local>
2020-05-11 20:05:54 +0530

    Modified settings.py

--- articlyzer/settings.py
+++ articlyzer/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import django_heroku
 import os
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'articlyzer.settings'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
