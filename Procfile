30acbe4e6a79eed4ebf0c22e841cb97d4eb7e832
Indrani Biswas <indranibiswas@Indranis-MacBook-Air.local>
2020-06-05 00:22:24 +0530

    Replaced spacy with nltk

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn articlyzer.wsgi && python -m spacy download en_core_web_sm
+web: gunicorn articlyzer.wsgi && python -m spacy download en_core_web_sm && python -m spacy link en_core_web_sm
--- articlyzer/views.py
+++ articlyzer/views.py
@@ -2,9 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from collections import Counter
 import math
-import spacy
+# import spacy
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
-nlp = spacy.load('en_core_web_sm')
+abc = nltk.download('all')
+# nlp = spacy.load('en_core_web_sm')
+sr = stopwords.words('english')
+sr.append('.')
 
 
 def home(request):
@@ -14,38 +20,67 @@ def home(request):
 def count(request):
     fulltext = request.POST['fulltext']
 
-    doc = nlp(fulltext)
+    # doc = nlp(fulltext)
 
-    lst_entities = []
-    lst_ent = []
-    set_lst_entities = set()
-    most_common_words = []
+    # lst_entities = []
+    # lst_ent = []
+    # set_lst_entities = set()
+    # most_common_words = []
 
     
-    words = [token.text for token in doc if token.is_stop != True and token.is_punct != True]
-    len_text = len(doc)
-    word_freq = Counter(words)
-    common_words = word_freq.most_common(5)
+    # words = [token.text for token in doc if token.is_stop != True and token.is_punct != True]
+    tokens = nltk.word_tokenize(fulltext)
+    len_text = len(tokens)
+
+    clean_tokens = tokens[:]
+
+    for token in tokens:
+        if token in sr:
+            print('token------->', token)
+            clean_tokens.remove(token)
+
+    freq = nltk.FreqDist(clean_tokens)
+    freq_cmn = freq.most_common(5)
+    tagged = nltk.pos_tag(tokens)
+    token_ent = nltk.ne_chunk(tagged, binary=True)
+
+    # for key, value in freq.most_common(5):
+    #     print(u'{}:{}'.format(key, value))
+    #     most_common_words.append(key)
+    #     most_common_words.append(value)
+
+    # len_text = len(doc)
+
+    print('fulltext------->', fulltext)
+    print('token_ent------>', token_ent)
+    print('len_text------->', len_text)
+    print('clean_tokens------->', clean_tokens)
+    print('tokens------->', tokens)
+    print('freq_cmn------->', freq_cmn)
+    
+
+    # word_freq = Counter(words)
+    # common_words = word_freq.most_common(5)
     # most_common_words = [k[0] for k in common_words]
-    # most_common_words_count = [k[1] for k in common_words]
-    for k in common_words:
-        most_common_words.append(k[0])
-        most_common_words.append(k[1])
+    # # most_common_words_count = [k[1] for k in common_words]
+    # for k in common_words:
+    #     most_common_words.append(k[0])
+    #     most_common_words.append(k[1])
 
-    print("common_words", common_words)
-    # print("most_common_words_count", most_common_words_count)
+    # print("common_words", common_words)
+    # # print("most_common_words_count", most_common_words_count)
     
-    if doc.ents:
-        for ent in doc.ents:
+    # if doc.ents:
+    #     for ent in doc.ents:
 
-            if ent.text in common_words[0]:
-                text = ent.text
-                # lst_ent.append(text)
-                if text not in set_lst_entities:
-                    lst_entities.append(text)
-                    set_lst_entities.add(text)
-                else:
-                    pass
+    #         if ent.text in common_words[0]:
+    #             text = ent.text
+    #             # lst_ent.append(text)
+    #             if text not in set_lst_entities:
+    #                 lst_entities.append(text)
+    #                 set_lst_entities.add(text)
+    #             else:
+    #                 pass
 
     val = math.ceil(len_text/200) * 60
 
@@ -60,6 +95,5 @@ def count(request):
         val = val/3600
 
     return render(request, 'home.html', {'count':len_text, \
-                'mostfrqwords':common_words, \
-                'time':val, 'unit': unit, 'fulltext': doc, \
-                'lst_entities': lst_entities})
+                'time':val, 'unit': unit, 'fulltext': fulltext, \
+                'mostfrqwords': freq_cmn})
